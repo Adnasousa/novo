a7420a3274dea299f0831578300a0c26c4943486
Adnasousa <102473567+Adnasousa@users.noreply.github.com>
2022-04-30 16:24:49 -0300

    Add files via upload

---
+++ Exercício.adna 03.py
@@ -0,0 +1,7 @@
+from logging import addLevelName
+
+
+primeiroNome = input("Digite seu primeiro nome: ")
+salárioAbril = input("Digite seu salário do mês de abril: ")
+mensagem = "O salário de "  + primeiroNome + "no  mês de abril foi de R$" + salárioAbril + " reais. "
+print(mensagem)
